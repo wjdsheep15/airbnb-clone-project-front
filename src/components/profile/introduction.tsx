ade61dd561692d8d5bfccaaadd1090b81482c918
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-25 14:26:39 +0900

    Feat: 이름 소개 및 수정하기 버튼 추가

---
+++ src/components/profile/introduction.tsx
@@ -0,0 +1,17 @@
+export default function introduction({ name }: { name: string }) {
+  return (
+    <div>
+      <h1 className="text-4xl font-extrabold dark:text-white ">
+        {name} 님 소개
+      </h1>
+      <button
+        type="button"
+        className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-1.5 text-center me-2 mb-2 mt-5 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800 font-bold"
+      >
+        프로필 수정하기
+      </button>
+      <br />
+      <br />
+    </div>
+  );
+}
