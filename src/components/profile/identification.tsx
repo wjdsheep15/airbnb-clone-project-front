433aebcd00887982469cf7d2f72edafe9cf57b9c
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-27 11:14:03 +0900

    Feat:사용자 신분 정보 박스 둥글게 다듬기

--- src/components/profile/identification.tsx
+++ src/components/profile/identification.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function identification({ name }: { name: string }) {
   return (
-    <span className="block w-[400px] h-[200px] p-6 bg-white border border-gray-200 rounded-e-3xl shadow-xl drop-shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <span className="relative block w-[350px] h-[200px] p-6 bg-white border border-gray-200 rounded-s-3xl  rounded-e-3xl shadow-xl drop-shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 right-[-10px]">
       <div className="flex">
         {/* 첫 번째 가로 칸 */}
-        <div className="w-2/3 mr-2 flex items-center justify-center">
+        <div className="w-2/3 mr-2 flex flex-col  items-center justify-center">
           <div>
             <Image
               className="rounded-full"
@@ -16,13 +16,13 @@ export default function identification({ name }: { name: string }) {
             />
           </div>
           <div>
-            <h2 className="text-xl font-bold ">{name}</h2>
-            <h1 className="text-xs"> 게스트</h1>
+            <h2 className="text-3xl font-bold ">{name}</h2>
           </div>
+          <h1 className="text-xs font-bold"> 게스트</h1>
         </div>
 
         {/* 두 번째 가로 칸 */}
-        <div className="w-1/3 ml-2 flex flex-col">
+        <div className="w-1/3 ml-2 flex flex-col ">
           {/* 첫 번째 세로 칸 */}
           <div className="h-1/2 mb-2">
             <h2 className="text-2xl">1</h2>
