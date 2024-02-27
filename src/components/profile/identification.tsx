1fd9b8a3b38be3d1e2df21530a63fb37371ffdc7
“Winsomejoo” <tmdwn0603@naver.com>
2024-02-27 11:36:36 +0900

    Feat: 사용자 사진변경 및 가입기간 위치 조정

--- src/components/profile/identification.tsx
+++ src/components/profile/identification.tsx
@@ -6,13 +6,13 @@ export default function identification({ name }: { name: string }) {
       <div className="flex">
         {/* 첫 번째 가로 칸 */}
         <div className="w-2/3 mr-2 flex flex-col  items-center justify-center">
-          <div>
+          <div className="mt-2">
             <Image
               className="rounded-full"
-              src="/images/profile1.png"
+              src="/images/profile-image.jpg"
               alt="image description"
-              width={60}
-              height={60}
+              width={120}
+              height={120}
             />
           </div>
           <div>
@@ -30,9 +30,10 @@ export default function identification({ name }: { name: string }) {
             <hr />
           </div>
           {/* 두 번째 세로 칸 */}
-          <div className="h-1/2"></div>
-          <h2 className="text-2xl">5</h2>
-          <p className="text-xs">에어비앤비 가입 기간(년)</p>
+          <div className="h-1/2 mb-4">
+            <h2 className="text-2xl">5</h2>
+            <p className="text-xs">에어비앤비 가입 기간(년)</p>
+          </div>
         </div>
       </div>
     </span>
