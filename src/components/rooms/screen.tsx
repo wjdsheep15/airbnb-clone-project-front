8214aed08e48c94a94897bd3b73563ad9440eff1
Minwoo Lee <rkdxhd97@naver.com>
2024-02-28 10:23:18 +0900

    형식 수정

--- src/components/rooms/screen.tsx
+++ src/components/rooms/screen.tsx
@@ -5,14 +5,12 @@ import RoomTitle from "@/components/rooms/room-title";
  * 전체 내용을 가지는 컴포넌트
  */
 export default function Screen() {
-  return (
-    <>  
-      <div className="flex flex-col max-w-[1120px]">
-        <div>
-            <RoomTitle />
-            <RoomAlbum id={0} />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col justify-center">
+                <RoomTitle />
+                <RoomAlbum id={0} />
+            </div>
+        </>
+    )
 }
