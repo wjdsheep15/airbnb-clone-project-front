bf2170dd977e121975adf986aef887ba663a5593
Minwoo Lee <rkdxhd97@naver.com>
2024-02-28 09:45:07 +0900

    Fix: 상세페이지 src 태그 컨벤션에 맞게 수정
    - 카멜케이스로 작성
    - mui import

--- src/components/rooms/room-title.tsx
+++ src/components/rooms/room-title.tsx
@@ -25,9 +25,9 @@ export default function RoomTitle() {
                     >
                       <path
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2M12 4v12m0-12 4 4m-4-4L8 8"
                       />
                     </svg>
@@ -45,9 +45,9 @@ export default function RoomTitle() {
                     >
                       <path
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"
                       />
                     </svg>
