da750a0b15d07050fee785641d472e7ee3e09851
“Winsomejoo” <tmdwn0603@naver.com>
2024-03-04 20:45:11 +0900

    Feat: user profile data fetch 구현

--- src/app/users/show/[id]/page.tsx
+++ src/app/users/show/[id]/page.tsx
@@ -3,24 +3,24 @@ import Identification from '@/components/profile/identification'
 import Introduction from '@/components/profile/introduction'
 import Review from '@/components/profile/review'
 
-export default async function UserProfilePage() {
-  const result = await fetch(`http://localhost:3000/api/user/1`)
+interface IdParams {
+  params: { id: number }
+}
+
+export default async function UserProfilePage({ params: { id } }: IdParams) {
+  const result = await fetch(`http://localhost:3000/api/user/${id}`)
   const inner = await result.json()
   const usersData = JSON.parse(inner.data)
-
-  usersData.map((eachUser: any) => {
-    console.log(eachUser)
-  })
 
   return (
     <section className='relative flex top-[40px] flex-wrap left-[100px] '>
       <div className='relative left-[200px] md:w-1/3 mb-4 md:mb-0'>
-        <Identification name='승주' />
-        <Authinfo name='승주' />
+        <Identification name={usersData.name} />
+        <Authinfo name={usersData.name} />
       </div>
       <div className='relative w-full md:w-2/3  '>
-        <Introduction name='승주' />
-        <Review name='승주' />
+        <Introduction name={usersData.name} />
+        <Review name={usersData.name} />
       </div>
     </section>
   )
