1f49164e35d703d71dae2cda7719588ef75c1e43
“Winsomejoo” <tmdwn0603@naver.com>
2024-03-04 19:43:08 +0900

    Feat:userData  API 구현

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,7 +1,18 @@
 import usersData from '../../../dummyDatas/usersData.json'
 
-export async function GET() {
+// /**
+//  *
+//  * @param {*} request
+//  * @param {*} param1
+//  * @returns
+//  */
+
+export async function GET(request, { params: { id: string } }) {
+  // const { searchParams } = new URL(request.url)
+  // const searchid = searchParams.get('searchid')
+
   const users = usersData.User
+
   const found = users.map((eachUser) => {
     const id = parseInt(eachUser.id)
     const name = eachUser.name
--- src/app/users/show/[id]/page.tsx
+++ src/app/users/show/[id]/page.tsx
@@ -4,6 +4,14 @@ import Introduction from '@/components/profile/introduction'
 import Review from '@/components/profile/review'
 
 export default async function UserProfilePage() {
+  const result = await fetch(`http://localhost:3000/api/user/1`)
+  const inner = await result.json()
+  const usersData = JSON.parse(inner.data)
+
+  usersData.map((eachUser: any) => {
+    console.log(eachUser)
+  })
+
   return (
     <section className='relative flex top-[40px] flex-wrap left-[100px] '>
       <div className='relative left-[200px] md:w-1/3 mb-4 md:mb-0'>
