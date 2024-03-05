19485ac733984bb45b60e693acdced4246c71fb3
“Winsomejoo” <tmdwn0603@naver.com>
2024-03-05 09:22:13 +0900

    Feat: 사용자가 인증 했을 때 유저프로필 인증컴포넌트 구현

--- src/app/users/show/[id]/page.tsx
+++ src/app/users/show/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Authinfo from '@/components/profile/authinfo'
 import Identification from '@/components/profile/identification'
 import Introduction from '@/components/profile/introduction'
+import IsAuthinfo from '@/components/profile/isAuthinfo'
 import Review from '@/components/profile/review'
 
 interface IdParams {
@@ -16,7 +17,12 @@ export default async function UserProfilePage({ params: { id } }: IdParams) {
     <section className='relative flex top-[40px] flex-wrap left-[100px] '>
       <div className='relative left-[200px] md:w-1/3 mb-4 md:mb-0'>
         <Identification name={usersData.name} />
-        <Authinfo name={usersData.name} />
+
+        {usersData.isAuth === true ? (
+          <IsAuthinfo name={usersData.name} />
+        ) : (
+          <Authinfo name={usersData.name} />
+        )}
       </div>
       <div className='relative w-full md:w-2/3  '>
         <Introduction name={usersData.name} />
