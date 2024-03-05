d9362c6df50172cc9702cc714cc985755e703c01
“Winsomejoo” <tmdwn0603@naver.com>
2024-03-05 10:27:32 +0900

    Feat:travelDesButton 파일 추가

---
+++ src/components/navigation/navibarButtons/travelDesButton.tsx
@@ -0,0 +1,11 @@
+export default function travelDesButton() {
+  const handleClick = () => {
+    alert('Div was clicked!')
+  }
+  return (
+    <div
+      className='flex w-20 h-12 bg-gradient-to-l from-mainColorHoverStart via-mainColorMiddle to-mainColorHoverEnd hover:bg-gradient-to-l hover:from-mainSearchColorStart hover:to-mainSearchColorEnd rounded-full items-center justify-center'
+      onClick={handleClick}
+    ></div>
+  )
+}
