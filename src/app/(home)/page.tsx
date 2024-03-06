76d88194be6fa5433319114433c8d0ec4476bc0e
wjdsheep15 <wjd15sheep@gmail.com>
2024-03-06 15:54:43 +0900

    Fix: delete GestNumber Components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import HeaderComponent from '@/app/(home)/header'
 import ItemCardPage from '@/components/itemCardView/itemCardPage'
-import GestNumber from '@/components/navigation/navibarButtons/gestNumber'
 
 export default async function HomePage() {
   return (
@@ -8,7 +7,6 @@ export default async function HomePage() {
       <main className='flex flex-col items-center justify-center w-full h-full'>
         <HeaderComponent />
         <ItemCardPage />
-        <GestNumber />
       </main>
     </>
   )
