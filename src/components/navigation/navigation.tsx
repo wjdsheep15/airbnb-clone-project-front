480782d06525b0d94d4706641129c063d9b05d29
wjdsheep15 <wjd15sheep@gmail.com>
2024-03-14 09:29:54 +0900

    Fix: scroll change move

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -8,11 +8,21 @@ import { useEffect, useState } from 'react'
 
 export default function NavigationBar() {
   const [scrollPosition, setScrollPosition] = useState(0)
+  const [isShrink, setIsShrink] = useState(false) // 네비게이션 바 축소 상태 관리
 
   // 스크롤 이벤트 리스너 함수입니다. 스크롤 시 호출됩니다.
   const handleScroll = () => {
     const position = window.pageYOffset
     setScrollPosition(position)
+
+    // 스크롤 위치에 따라 네비게이션 바의 축소 상태를 변경합니다.
+    if (!isShrink && position > 150) {
+      // 스크롤이 아래로 내려갔을 때 축소 상태로 변경
+      setIsShrink(true)
+    } else if (isShrink && position < 50) {
+      // 스크롤이 위로 올라갔을 때 원래 크기로 복귀
+      setIsShrink(false)
+    }
   }
 
   // 컴포넌트 마운트 시 스크롤 이벤트 리스너를 추가하고, 언마운트 시 제거합니다.
@@ -22,11 +32,11 @@ export default function NavigationBar() {
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
-  }, [])
+  }, [isShrink]) // 의존성 배열에 isShrink 추가
 
   return (
     <div
-      className={`flex w-full  justify-center ${scrollPosition > 100 ? 'sm:h-20 md:h-20 lg:h-20 xl:h-20 2xl:h-20' : 'sm:h-60 md:h-60 lg:h-40 xl:h-40 2xl:h-40'} `}
+      className={`flex w-full justify-center ${isShrink ? 'sm:h-20 md:h-20 lg:h-20 xl:h-20 2xl:h-20' : 'sm:h-60 md:h-60 lg:h-40 xl:h-40 2xl:h-40'} `}
     >
       <div className='flex w-[91.16%] h-20'>
         <div className='flex w-full h-20 justify-center items-center relative'>
@@ -41,10 +51,10 @@ export default function NavigationBar() {
           {/* 중간 검색 구역 */}
 
           <div
-            className={`flex grow top-0  absolute translate(-50%, -50%) sm:flex  md:flex lg:flex xl:flex 2xl:flex ${scrollPosition > 100 ? 'sm:top-0 md:top-0  lg:top-0  xl:top-0  2xl:top-0' : 'sm:top-14 md:top-14  lg:top-0  xl:top-0  2xl:top-0'} `}
+            className={`flex grow top-0  absolute translate(-50%, -50%) sm:flex  md:flex lg:flex xl:flex 2xl:flex ${isShrink ? 'sm:top-0 md:top-0  lg:top-0  xl:top-0  2xl:top-0' : 'sm:top-14 md:top-14  lg:top-0  xl:top-0  2xl:top-0'} `}
           >
             <div className='flex flex-col min-h-20 items-center justify-center'>
-              {scrollPosition > 100 ? <RoomSearchNavigation /> : <HomeNavigation />}
+              {isShrink ? <RoomSearchNavigation /> : <HomeNavigation />}
             </div>
           </div>
 
