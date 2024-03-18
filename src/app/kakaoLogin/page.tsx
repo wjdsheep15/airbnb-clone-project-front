ba3e4020eae8ba0a0e417b3202ef4f9b63efa3ee
“Winsomejoo” <tmdwn0603@naver.com>
2024-03-18 19:59:49 +0900

    Fix: Kakao Login Redirect Error 해결

---
+++ src/app/kakaoLogin/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import KakaoLogin from '@/components/kakaoLogin/KakaoLogin'
+import { Fragment } from 'react'
+
+const KakaoLoginPage = () => {
+  return 'redirect'
+}
+
+export default KakaoLogin
